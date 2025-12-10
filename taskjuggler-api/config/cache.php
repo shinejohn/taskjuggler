087707e11402f0c5c6f98c6ee9f9020ecdaed262
shinejohn <john_shine@hotmail.com>
2025-12-10 11:24:15 -0500

    Fix cache default: Change from redis to file

--- taskjuggler-api/config/cache.php
+++ taskjuggler-api/config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'redis')),
+    'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'file')),
 
     /*
     |--------------------------------------------------------------------------
