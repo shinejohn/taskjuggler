38c9bcb21cbc997b9318041d7793745dc30e7e36
shinejohn <john_shine@hotmail.com>
2026-01-12 15:18:59 -0500

    Fix shared-ui import paths - correct relative paths to lib/utils and components

--- shared-ui/src/components/ui/sheet/SheetContent.vue
+++ shared-ui/src/components/ui/sheet/SheetContent.vue
@@ -11,7 +11,7 @@ import {
   DialogPortal,
   useForwardPropsEmits,
 } from "reka-ui"
-import { cn } from "../../lib/utils"
+import { cn } from "../../../../lib/utils"
 import { sheetVariants } from "."
 
 interface SheetContentProps extends DialogContentProps {
